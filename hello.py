50ac271b84797bb8b9cc5386951a6be52eb44191
Atsutoshi Kamijo <k1810178@uec.ac.jp>
2020-07-23 18:27:36 +0900

    休憩や

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-l  = [10, 20, 30, 40, 50]
-l
-l[2:4]
+indxs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+fname_tmpl = 'data.%02d'
+for i in indxs:
+    fname = fname_tmpl % i
+    print(fname)          # ためしに fname を表示させてみる．
+    # ここに fname を使った処理を書いていく
