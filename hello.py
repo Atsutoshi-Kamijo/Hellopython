bd735bb1742f733099873b2ba958d3255751b47f
Atsutoshi Kamijo <k1810178@uec.ac.jp>
2020-07-25 03:41:41 +0900

    夜や

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-indxs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-fname_tmpl = 'data.%02d'
-for i in indxs:
-    fname = fname_tmpl % i
-    print(fname)          # ためしに fname を表示させてみる．
-    # ここに fname を使った処理を書いていく
+a=2
+if a==1:
+	print('a is 1')
+elif a==2:
+	print('a is 2')
+else:
+	print('more')
